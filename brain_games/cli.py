cf6b3338fd2592908813bc95ad3a439fb1aa0264
gato <alex.n.parshikov@gmail.com>
2020-07-25 19:49:29 +0300

    checked files after the linter

---
+++ brain_games/cli.py
@@ -0,0 +1,10 @@
+# Command line interface for brain-games.
+
+
+import prompt
+
+
+def welcome_user():
+    # Get an user name and promt user.:
+    name = prompt.string('May I have your name? ')
+    print('Hello, {0}!'.format(name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,14 @@
+# Main programm.
+
+
+from brain_games.cli import welcome_user
+
+
 def main():
+    # Make a user intreface.
     print('Welcome to the Brain Games!')
+    welcome_user()
 
 
 if __name__ == '__main__':
-   main()
-
+    main()
