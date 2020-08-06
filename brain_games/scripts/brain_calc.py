586e3a8614656c49efe3d7cdfa058bc01ee2c2e1
gato <alex.n.parshikov@gmail.com>
2020-08-06 23:06:07 +0300

    Fixed error after linter

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -6,7 +6,7 @@ from brain_games.calculator import game_calculator
 
 def main():
     # Make a user intreface.
-    print('Welcome to the Brain Games!')    
+    print('Welcome to the Brain Games!')
     game_calculator()
 
 
