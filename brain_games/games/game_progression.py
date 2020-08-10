9abe708cedd17af5596c411e56f1ae2405a5df37
gato <alex.n.parshikov@gmail.com>
2020-08-10 12:59:43 +0300

    Added game of progression

---
+++ brain_games/games/game_progression.py
@@ -0,0 +1,16 @@
+import random
+
+
+def calc_progression():
+    # Formation of the progression and definition of a lost item
+    list = [random.randint(1, 10)]
+    step = random.randint(1, 10)
+    lostitemindex = random.randint(1, 10)
+    i = 1
+    while i < 10:
+        list.append(list[i - 1] + step)
+        i += 1
+    lostitem = list[lostitemindex - 1]
+    list[lostitemindex - 1] = '..'
+    print('Question: ', *list)
+    return lostitem
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,14 @@
+# Main program for game of progression.
+
+
+from brain_games.progression import game_progression
+
+
+def main():
+    # Make a user intreface.
+    print('Welcome to the Brain Games!')
+    game_progression()
+
+
+if __name__ == '__main__':
+    main()
